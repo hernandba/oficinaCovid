3c996bd9d6c0f2d3e0cab454996c64eaf1318f6e
YeisonHurtado <yeisonhurtado73@gmail.com>
2021-09-25 16:43:24 -0500

    Agregue métodos de inserción y consulta, en la capa Consola.

--- oficinaCovid.App/oficinaCovid.App.Consola/Program.cs
+++ oficinaCovid.App/oficinaCovid.App.Consola/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using oficinaCovid.App.Dominio;
+using oficinaCovid.App.Persistencia;
 
 namespace oficinaCovid.App.Consola
 {
     class Program
     {
+        private static IRepositorioGobernadorAsesor _repoGobernador = new RepositorioGobernadorAsesor(new Persistencia.AppContext());
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            AddGobernador();
+            EncontrarGobernador(1);
+        }
+
+        // CRUD 
+        // Adicionar Gobernador/Asesor
+        private static void AddGobernador()
+        {
+            var gobernador = new GobernadorAsesor
+            {
+                identificacion = "10052323999",
+                nombres = "Jeison",
+                apellidos = "Hurtado",
+                edad = 20,
+                genero = "Masculino",
+                diagnostico = null,
+                rol = "Asesor"
+            };
+            _repoGobernador.AddGA(gobernador);
+        }
+
+        // Encontrar dato
+        private static void EncontrarGobernador(int idGobernador)
+        {
+            var gobernadorEncontrado = _repoGobernador.GetGA(idGobernador);
+            Console.WriteLine(gobernadorEncontrado.nombres + " " + gobernadorEncontrado.apellidos);
         }
     }
 }
