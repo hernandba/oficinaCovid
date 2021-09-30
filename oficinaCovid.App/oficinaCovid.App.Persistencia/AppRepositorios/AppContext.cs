62db6fb5878d4bcd37de915b24e5b4cd5d0cf2b8
hernandba <hernandba@gmail.com>
2021-09-30 18:34:23 -0500

    Cambio de nombre entidades, creacion AppContext en Persistencia

---
+++ oficinaCovid.App/oficinaCovid.App.Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using oficinaCovid.App.Dominio;
+
+namespace oficinaCovid.App.Persistencia
+{
+    public class AppContext : DbContext
+    {
+        //Se debe crear una propiedad por cada una de las entidades del dominio
+        public DbSet<Persona> Personas { get; set; }
+        public DbSet<Diagnostico> Diagnosticos { get; set; }
+        public DbSet<Sintomatologia> Sintomatologias { get; set; }
+        public DbSet<Aseador> Aseadores { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<Administrativo> Administrativos { get; set; }
+        public DbSet<Secretario> Secretarios { get; set; }
+        public DbSet<Sede> Sedes { get; set; }
+        public DbSet<Oficina> Oficinas { get; set; }
+        public DbSet<Disponibilidad> Disponibilidades { get; set; }
+
+        //Funcion que configura la conexion con la base de datos
+        protected override void onConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = OficinaCovid.Data");
+																									//Nombre del Servidor             Nombre de la BD
+            }
+        }
+    }
+}
