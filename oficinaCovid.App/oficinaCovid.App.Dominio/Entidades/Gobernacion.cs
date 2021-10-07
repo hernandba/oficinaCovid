eea9cdec7d8a9c82e8e655866162db7c7df9d5ba
Remuda2021 <OmarEsper58@gmail.com>
2021-10-06 20:02:31 -0500

    Agregar comentario

--- oficinaCovid.App/oficinaCovid.App.Dominio/Entidades/Gobernacion.cs
+++ oficinaCovid.App/oficinaCovid.App.Dominio/Entidades/Gobernacion.cs
@@ -4,6 +4,7 @@ namespace oficinaCovid.App.Dominio
 {
     public class Gobernacion
     {
+        //atributos
         public int id { get; set; }
         public string nombre { get; set; }
         public string ciudad { get; set; }
